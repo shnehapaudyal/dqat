901557835baab2fc30c2a7774dd7c32c8f820afb
Shneha Paudyal <eshnehapaudyal@gmail.com>
2024-06-30 15:52:53 +0100

    Add react module

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'static/File'
 
+
 class UploadFileForm(FlaskForm):
     file = FileField("File", validators=[InputRequired()])
     submit = SubmitField("Upload File")
 
-@app.route('/', methods=['GET',"POST"])
-@app.route('/home', methods=['GET',"POST"])
+
+@app.route('/', methods=['GET', "POST"])
+@app.route('/home', methods=['GET', "POST"])
 def home():
     form = UploadFileForm()
     if form.validate_on_submit():
@@ -24,6 +26,7 @@ def home():
         file.save(file_path)  # Then save the file
         return "File has been uploaded."
     return render_template('index.html', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
