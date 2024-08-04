6ba1159ee6d404ab0be1c9128cba27452f322b66
Shneha <eshnehapaudyal@gmail.com>
2024-08-04 14:22:44 +0100

    Add outlier domain

--- datasets.py
+++ datasets.py
@@ -34,13 +34,13 @@ def get_inconsistent_datatype(dataset_id):
 def get_outlier(dataset_id):
     dataset_path = db.read_dataset(dataset_id).path
     df = files.read(dataset_path)
-    return definemetrics.outliers(df)
+    return domain.outlier.outliers(df)
 
 
 def get_typos(dataset_id):
     dataset_path = db.read_dataset(dataset_id).path
     df = files.read(dataset_path)
-    return definemetrics.typos(df)
+    return domain.readability.typos(df)
 
 
 def get_formats(dataset_id):
@@ -53,5 +53,6 @@ def get_duplicate(dataset_id):
     dataset_path = db.read_dataset(dataset_id).path
     df = pd.read_csv(dataset_path)
     duplicates_info = definemetrics.duplicate_records(df)
-    duplicates_info = {key: float(value) if isinstance(value, (int, float)) else value for key, value in duplicates_info.items()}
+    duplicates_info = {key: float(value) if isinstance(value, (int, float)) else value for key, value in
+                       duplicates_info.items()}
     return jsonify(duplicates_info)
