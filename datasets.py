Fix conversion to number type for stat calculation

datasets.py
@@ -7,6 +7,7 @@ import db
 from domain import types
 import pickle
 
+
 def get_dataset(dataset_id):
     return db.read_dataset(dataset_id)
 
@@ -16,25 +17,26 @@ def get_datatypes(df):
 
 
 def statistics(df):
+
     column_types = get_datatypes(df).to_dict(orient='list')
 
     def try_float(value):
         try:
-            return float(value)
+            return float(str(value).replace(',', ''))
         except ValueError:
             return np.nan
 
     def is_numeric(column):
         return column_types['type'][column_types['column'].index(column)] in ['float', 'integer']
 
-    stats = df.describe(include='all')
-
     for column in df.columns:
-        df[column] = df[column].map(lambda x: try_float(x) if is_numeric(column) else x)
+        is_numeric_column = is_numeric(column)
+        df[column] = df[column].map(lambda x: try_float(x) if is_numeric_column else x)
+
+    stats = df.describe(include='all')
 
     stats = stats.fillna(np.nan).replace([np.nan], [None])
     return stats.to_json()
-
 
 
 try: